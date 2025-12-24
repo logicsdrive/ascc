a30cefff450db754f90d8aeca5395c64f8b0fe23
ashfaq <ashfaq@carswitch.com>
2025-12-24 14:53:29 +0500

    ui adjustments

---
+++ images/generic_svg_assets.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="swiper_arrow" viewBox="0 0 46 46">
+        <circle cx="23" cy="23" r="21.5" stroke="currentColor" stroke-width="2"/>
+        <path d="M25.2 17.25L19.85 22.5L25.2 27.75" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/>
+    </symbol>
+</svg>
---
+++ images/home/svg_assets.php
@@ -0,0 +1,5 @@
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="readmore_icon" viewBox="0 0 24 28">
+        <path d="M9.91406 16.8874L13.6705 13.5134L9.91406 10.1394" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </symbol>
+</svg>
